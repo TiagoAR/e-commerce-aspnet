1e15fe550dc249de2ffb20ecd4997f1e16b319e9
tiago <tiago@tiago.com.br>
2020-06-10 17:12:52 -0400

    adding pagination sorting searching capacity and CORS

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -10,6 +10,7 @@ using AutoMapper;
 using API.Dtos;
 using API.Errors;
 using Microsoft.AspNetCore.Http;
+using API.Helpers;
 
 namespace API.Controllers
 {
@@ -31,11 +32,20 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts()
+        public async Task<ActionResult<Pagination<ProductToReturnDto>>> GetProducts([FromQuery] ProductSpecParams productParams)
         {
-            var spec = new ProductsWithTypesAndBrandsSpecification();
+            var spec = new ProductsWithTypesAndBrandsSpecification(productParams);
+
+            var countSpec = new ProductsWithFiltersForCountSpecification(productParams);
+
+            var totalItems = await _productRepo.CountAsync(countSpec);
+
             var products = await _productRepo.ListAsync(spec);
-            return Ok(_mapper.Map<IReadOnlyList<Product>,IReadOnlyList<ProductToReturnDto>>(products));
+
+            var data = _mapper.Map<IReadOnlyList<Product>,IReadOnlyList<ProductToReturnDto>>(products);
+
+            return Ok(new Pagination<ProductToReturnDto>(productParams.PageIndex,
+                productParams.PageSize,totalItems,data));
         }
 
         [HttpGet("{id}")]
--- Core/Specifications/BaseSpecifications.cs
+++ Core/Specifications/BaseSpecifications.cs
@@ -6,23 +6,48 @@ namespace Core.Specifications
 {
     public class BaseSpecifications<T> : ISpecification<T>
     {
+
+        public Expression<Func<T, bool>> Criteria { get; }
+
+        public List<Expression<Func<T, object>>> Includes { get; } =
+            new List<Expression<Func<T, object>>>();
+        public Expression<Func<T, object>> OrderBy { get; private set; }
+
+        public Expression<Func<T, object>> OrderByDescending { get; private set; }
+        
+        public int Take {get; private set;}
+
+        public int Skip {get; private set;}
+
+        public bool IsPagingEnabled {get; private set;}
         public BaseSpecifications()
         {
         }
 
-        public BaseSpecifications(Expression<Func<T,bool>> criteria)
+        public BaseSpecifications(Expression<Func<T, bool>> criteria)
         {
             Criteria = criteria;
         }
+        protected void AddOrderBy(Expression<Func<T, object>> orderByExpression)
+        {
+            OrderBy = orderByExpression;
+        }
 
-        public Expression<Func<T, bool>> Criteria {get;}
+        protected void AddOrderByDescending(Expression<Func<T, object>> orderByDescExpression)
+        {
+            OrderByDescending = orderByDescExpression;
+        }
 
-        public List<Expression<Func<T, object>>> Includes {get;} = 
-            new List<Expression<Func<T,object>>>();
-
-        protected void AddInclude(Expression<Func<T,object>> includeExpression)
+        protected void AddInclude(Expression<Func<T, object>> includeExpression)
         {
             Includes.Add(includeExpression);
+        }
+
+        protected void ApplyPaging(int skip, int take)
+        {
+            Skip = skip;
+            Take = take;
+            IsPagingEnabled = true;
         }
     }
 }
